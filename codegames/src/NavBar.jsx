ed3a065d73195539919c5019869c718552465a68
Samuel Adelson <adelsonllc@gail.com>
2023-01-05 12:40:38 -0500

    links on Navbar

--- codegames/src/NavBar.jsx
+++ codegames/src/NavBar.jsx
@@ -2,13 +2,20 @@ import ScoreCard from "./ScoreCard";
 import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
-    // let navigate = useNavigate();
+  let navigate = useNavigate();
 
   return (
     <div className="header">
       {/* <img src="src/logo.svg" id="navimg"/> */}
       <div className="codegames">CodeGames</div>
-      <button className="navbutton">High Scores</button>
+      <button
+        className="navbutton"
+        onClick={() => {
+          navigate("/scores");
+        }}
+      >
+        High Scores
+      </button>
       <p></p>
       <button className="navbutton">Home</button>
     </div>
